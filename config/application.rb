43652136bd393c627f52f3778f5893be1d19f134
Alek Grinberg <agrinb@gmail.com>
2013-12-18 19:03:29 -0500

    Added login/logout functionality

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(:default, Rails.env)
 
 module Pinteresting
   class Application < Rails::Application
+
+#required by heroku
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
